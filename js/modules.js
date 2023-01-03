a892be1e2125776f01aaa31300e5818cbd36f857
Pedroqne <pedro.conceicao2014@gmail.com>
2023-01-03 18:55:37 -0300

    ScrollSuave transformado em classe

--- js/modules.js
+++ js/modules.js
@@ -1,6 +1,6 @@
 import navTab from './navTab.js';
 import initAccordion from './initAccordion.js';
-import scrollSuave from './scrollSuave.js';
+import ScrollSuave from './scrollSuave.js';
 import initModal from './modal.js';
 import initTooltip from './toolTip.js';
 import initDropdownMenu from './dropdown.js';
@@ -9,9 +9,13 @@ import initAnimaScroll from './animaScroll.js';
 import initAnimaNumeros from './animaNumeros.js';
 import initFuncionamento from './funcionamento.js';
 
+const options = { behavior: 'smooth', block: 'start' };
+
+const scrollSuave = new ScrollSuave('.js-menu a[href^="#"]', options);
+scrollSuave.init();
+
 navTab();
 initAccordion();
-scrollSuave();
 initModal();
 initTooltip();
 initDropdownMenu();
